1252a7b1c73cf7cb455de06a1ec5aa5f42e03e33
redwan100 <78641603+redwan100@users.noreply.github.com>
2021-02-19 14:14:30 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,11 @@
+function imageSlider(anything) {
+    const starbuck = document.querySelector('.starbuck');
+    // document.querySelector('.starbuck').src = anything;
+    starbuck.src = anything;
+}
+
+function changeColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+
